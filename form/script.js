6e320a2b7c47ab45f8eb5b2ccea132ee0d4103d9
anish <anishgautam2024@gmail.com>
2024-11-25 20:38:24 +0545

    updated script for index page

--- form/script.js
+++ form/script.js
@@ -1,9 +1,11 @@
 const baseapi_url = "https://rest-api-7esz.onrender.com/api/user";
 
-function showDiv() {
-  document.getElementById("responsetable").style.display = "table";
-}
-
+const checkUser = () => {
+  const access_Token = localStorage.getItem("accessToken");
+  if (access_Token) {
+    window.location.href = "./allusers.html";
+  }
+};
 async function handleSubmit(event) {
   event.preventDefault();
   document.getElementById("loader").style.display = "block";
@@ -28,7 +30,8 @@ async function handleSubmit(event) {
       errorbox.style.display = "none";
       document.getElementById("loader").style.display = "none";
       alert("Form submitted successfully!");
-      allUsers(); // Refresh the table with the latest data
+      localStorage.setItem("accessToken", result.accessToken);
+      window.location.href = "./allusers.html";
     } else {
       document.getElementById("loader").style.display = "none";
       console.error("Error submitting form:", response.status);
@@ -43,44 +46,4 @@ async function handleSubmit(event) {
   }
 }
 
-async function allUsers() {
-  try {
-    const response = await fetch(`${baseapi_url}/all`);
-    const result = await response.json();
-
-    if (response.ok) {
-      console.log("All users:", result.data);
-
-      // Clear the table before adding new rows
-      const tableBody = document.querySelector("#responsetable tbody");
-      tableBody.innerHTML = ""; // Clear any previous rows
-      let count = 1;
-      result.data.forEach((user) => {
-        const row = tableBody.insertRow();
-        const snCell = row.insertCell();
-        const nameCell = row.insertCell();
-        const emailCell = row.insertCell();
-        const statusCell = row.insertCell();
-        snCell.innerText = count;
-        nameCell.innerText = user.name;
-        emailCell.innerText = user.email;
-        statusCell.innerText = user.status ? "Active" : "Success";
-        statusCell.style.color = "green";
-
-        count++;
-      });
-
-      // Make the table visible
-      showDiv();
-    } else {
-      console.error("Error fetching users:", response.status);
-      alert("Failed to fetch users!");
-    }
-  } catch (error) {
-    console.error("Error during fetch:", error);
-    alert("An error occurred while fetching all users.");
-  }
-}
-
-// Fetch all users when the page loads
-window.onload = allUsers;
+window.onload = checkUser;
